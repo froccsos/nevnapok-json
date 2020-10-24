fce1ea930fef9d3d942413f8f9609e8b9888dfa8
Daniel Belso <daniel.belso@jfweb.work>
2020-10-24 21:53:54 +0200

    Extend test with echo + var_dump.

--- test.php
+++ test.php
@@ -4,5 +4,6 @@ require 'NameDay.php';
 
 $nameDay = new NameDay();
 $nameDay::refresh();
-$nameDay::get(1,1);
+echo var_dump($nameDay::get(1,1)['main']);
+echo var_dump($nameDay::get(12,6)['main']);
 $nameDay::get(13,1);
